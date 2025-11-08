0e28e897cfa5969451da02aa0e81b8d0fab487fc
Fernando <palhares.f@gmail.com>
2025-11-08 15:08:04 +0100

    Challenge for 20251108 done

---
+++ python/2025/11/20251108.py
@@ -0,0 +1,27 @@
+# Character Limit
+
+# In this challenge, you are given a string and need to determine if it fits in a social media post. Return the following strings based on the rules given:
+
+#     "short post" if it fits within a 40-character limit.
+#     "long post" if it's greater than 40 characters and fits within an 80-character limit.
+#     "invalid post" if it's too long to fit within either limit.
+
+# Tests
+
+# 1. can_post("Hello world") should return "short post".
+# 2. can_post("This is a longer message but still under eighty characters.") should return "long post".
+# 3. can_post("This message is too long to fit into either of the character limits for a social media post.") should return "invalid post".
+
+def can_post(message):
+    if(len(message) <= 40):
+        return "short post"
+    elif(len(message) > 40 and len(message) <= 80):
+        return "long post"
+    else:
+        return "invalid post"
+
+can_post("Hello world")
+can_post("This is a longer message but still under eighty characters.")
+can_post("This message is too long to fit into either of the character limits for a social media post.")
+
+# Comments - pretty easy challenge, not much to say here
